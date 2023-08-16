20eb56881d5d1c55517bffa714e92c66ca1ea2f6
Inuri Lavanya <lilh99lilh@gmail.com>
2023-08-16 13:45:08 +0530

    Drag and drop functionality of the linked list nodes

--- Algorithmia/Assets/Scripts/Linked Lists/Linkedlistblockmanager.cs
+++ Algorithmia/Assets/Scripts/Linked Lists/Linkedlistblockmanager.cs
@@ -13,7 +13,17 @@ public class Linkedlistblockmanager : MonoBehaviour
     [SerializeField]
     private GameObject linkedListPrefab;
 
+    [SerializeField]
     GameObject currentObj;
+
+    [SerializeField]
+    private LayerMask anotherLayer;
+
+    [SerializeField]
+    private LayerMask dataLayer;
+
+    [SerializeField]
+    private Transform dataParentObj;
 
     private float startPosX;
 
@@ -22,6 +32,9 @@ public class Linkedlistblockmanager : MonoBehaviour
     private int workspaceLayer;
 
     private ArrayBlockList linkedListblocksList;
+
+    [SerializeField]
+    private ArrayLevelManager levelManager;
 
 
     // Start is called before the first frame update
@@ -56,6 +69,54 @@ public class Linkedlistblockmanager : MonoBehaviour
                     startPosX = mousePos.x - currentObj.transform.position.x;
                     startPosY = mousePos.y - currentObj.transform.position.y;
                 }
+                /*else if (hit.collider.name == "Right Arrow")
+                {
+                    currentObj = Instantiate(linkedListblocksList.blockList["Right Arrow"], new Vector3(mousePos.x, mousePos.y, 0f), Quaternion.identity);
+                    startPosX = mousePos.x - currentObj.transform.position.x;
+                    startPosY = mousePos.y - currentObj.transform.position.y;
+                }*/
+            } 
+            else
+            {
+                RaycastHit2D dataHit = Physics2D.Raycast(mousePos, Vector3.zero, 20f, dataLayer);
+
+                if (dataHit.collider != null)
+                {
+                    currentObj = dataHit.collider.gameObject;
+
+                    startPosX = mousePos.x - currentObj.transform.position.x;
+                    startPosY = mousePos.y - currentObj.transform.position.y;
+                }
+                else
+                {
+                    RaycastHit2D[] allhits = Physics2D.RaycastAll(mousePos, Vector3.zero, Mathf.Infinity, anotherLayer);
+
+
+                    if (allhits.Length > 1)
+                    {
+                        foreach (RaycastHit2D singleHit in allhits)
+                        {
+                            if (singleHit.collider.CompareTag("Data"))
+                            {
+                                currentObj = singleHit.collider.gameObject;
+                            }
+                        }
+                    }
+                    else if (allhits.Length == 1)
+                    {
+
+                        if (allhits[0].collider.CompareTag("Inventory"))
+                        {
+                            currentObj = allhits[0].collider.gameObject;
+                        }
+                    }
+
+                    if (currentObj != null)
+                    {
+                        startPosX = mousePos.x - currentObj.transform.position.x;
+                        startPosY = mousePos.y - currentObj.transform.position.y;
+                    }
+                }
             }
         }
 
@@ -72,16 +133,161 @@ public class Linkedlistblockmanager : MonoBehaviour
 
         if (Input.GetMouseButtonUp(0))
         {
-            if (currentObj != null && currentObj.GetComponent<LinkedListBlock>().inWorkspace == true)
+            if (currentObj != null && currentObj.CompareTag("Inventory") && currentObj.GetComponent<LinkedListBlock>().inWorkspace == true)
             {
-                currentObj.layer = workspaceLayer;
+
+                if (currentObj.layer != workspaceLayer)
+                {
+
+                    TrackSnapPoints(currentObj);
+                    //TrackLinePoints(currentObj);
+                    ChangeBlockLayer(currentObj.transform, "Workspace");
+                    //levelManager.blockCount += 1;
+                    
+                }
+
+
                 currentObj = null;
             }
-            else
+            else if (currentObj != null && currentObj.CompareTag("Inventory") && currentObj.GetComponent<ArrayBlock>().inWorkspace == false)
             {
-                Destroy(currentObj);
+                DestroyBlocks(currentObj);
             }
+            else if (currentObj != null && currentObj.CompareTag("Data"))
+            {
 
+                float _snapRadius = currentObj.GetComponent<DataBlock>().snapRadius;
+
+                currentObj.GetComponent<DataBlock>().snapped = false;
+
+                for (int i = 0; i < levelManager.correctForms.Count; i++)
+                {
+                    if ((Mathf.Abs(currentObj.transform.position.x - levelManager.correctForms[i].transform.position.x) <= _snapRadius &&
+                    Mathf.Abs(currentObj.transform.position.y - levelManager.correctForms[i].transform.position.y) <= _snapRadius) &&
+                    levelManager.correctForms[i].transform.childCount == 0)
+                    {
+                        currentObj.transform.position = new Vector3(levelManager.correctForms[i].transform.position.x, levelManager.correctForms[i].transform.position.y, 0f);
+                        currentObj.GetComponent<DataBlock>().snapped = true;
+
+                        ChangeBlockLayer(currentObj.transform, "Workspace");
+
+                        //make the data element a child of the snapped point
+                        currentObj.transform.SetParent(levelManager.correctForms[i].transform);
+
+                        currentObj.transform.localScale = new Vector3(1f, 1f, 0f);
+
+                        break;
+                    }
+                }
+                if (currentObj.GetComponent<DataBlock>().snapped == false)
+                {
+                    Vector3 currentResetPos = currentObj.GetComponent<DataBlock>().resetPosition;
+                    currentObj.transform.position = new Vector3(currentResetPos.x, currentResetPos.y, currentResetPos.z);
+                    currentObj.transform.SetParent(dataParentObj);
+                    ChangeBlockLayer(currentObj.transform, "Data");
+                    currentObj.transform.localScale = currentObj.GetComponent<DataBlock>().originalScale;
+
+                }
+
+                currentObj = null;
+            }
         }
     }
+      
+    private void TrackSnapPoints(GameObject parentObj)
+    {
+        Transform snapPointsListObj = parentObj.transform.Find("Snap Points");
+
+        if (snapPointsListObj != null)
+        {
+            Transform[] snapPointsList = snapPointsListObj.GetComponentsInChildren<Transform>(includeInactive: false);
+
+            foreach (Transform snapPoint in snapPointsList)
+            {
+                if (snapPoint != snapPointsListObj)
+                {
+                    levelManager.correctForms.Add(snapPoint.gameObject);
+                }
+
+            }
+        }
+    }
+
+    /* private void TrackLinePoints(GameObject parentObj)
+    {
+
+        Transform linePoints = parentObj.transform.Find("Line Points");
+
+        if (linePoints != null)
+         {
+             Transform endPoint = linePoints.Find("Line End");
+             {
+                 levelManager.lineEndPoints.Add(endPoint);
+             }
+         }
+    }*/
+
+    private void ChangeBlockLayer(Transform currentObj, string layerName)
+    {
+        if (currentObj != null)
+        {
+            currentObj.gameObject.layer = LayerMask.NameToLayer(layerName);
+        }
+
+        for (int i = 0; i < currentObj.childCount; i++)
+        {
+            Transform childTransform = currentObj.GetChild(i);
+            ChangeBlockLayer(childTransform, layerName);
+        }
+    }
+
+    private void DestroyBlocks(GameObject currentObj)
+    {
+        Transform snapPointsListObj = currentObj.transform.Find("Snap Points");
+        //Transform linePointsObj = currentObj.transform.Find("Line Points");
+        int deletedCount = 0;
+        //int deletedLines = 0;
+
+        if (snapPointsListObj != null)
+        {
+            Transform[] snapPointsList = snapPointsListObj.GetComponentsInChildren<Transform>(includeInactive: false);
+
+
+            for (int i = 0; i < snapPointsList.Length; i++)
+            {
+                for (int j = 0; j < levelManager.correctForms.Count; j++)
+                {
+                    if (snapPointsList[i].gameObject == levelManager.correctForms[j])
+                    {
+                        levelManager.correctForms.RemoveAt(j);
+                        deletedCount += 1;
+                    }
+                }
+            }
+        }
+
+        /*if (linePointsObj != null)
+        {
+            Transform[] linePointsList = linePointsObj.GetComponentsInChildren<Transform>(includeInactive: false);
+
+            for (int i = 0; i < linePointsList.Length; i++)
+            {
+                for (int j = 0; j < levelManager.lineEndPoints.Count; j++)
+                {
+                    if (linePointsList[i] == levelManager.lineEndPoints[j])
+                    {
+                        levelManager.lineEndPoints.RemoveAt(j);
+                        deletedLines += 1;
+                    }
+                }
+            }
+        }
+
+        if (deletedCount == 4 || deletedLines == 1)
+        {
+            levelManager.blockCount -= 1;
+            Destroy(currentObj);
+        }*/
+    }
 }
+
