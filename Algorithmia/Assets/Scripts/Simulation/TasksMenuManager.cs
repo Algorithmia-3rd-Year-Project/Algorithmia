59cab10396916a4f83194fb7e26095d4a385003c
Kavindu <kavindupriyanath@gmail.com>
2023-10-02 10:27:22 +0530

    Leisure activity menus set

--- Algorithmia/Assets/Scripts/Simulation/TasksMenuManager.cs
+++ Algorithmia/Assets/Scripts/Simulation/TasksMenuManager.cs
@@ -27,7 +27,7 @@ public class TasksMenuManager : MonoBehaviour
     {
         ReadABookButton();
         LibraryMembership();
-        UpdateLibraryMembership();
+        //UpdateLibraryMembership();
     }
 
     private void ReadABookButton()
