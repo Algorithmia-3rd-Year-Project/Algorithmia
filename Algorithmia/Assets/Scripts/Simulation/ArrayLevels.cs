c0be4638957f2ff64052b67153930f44877d25e5
Kavindu <kavindupriyanath@gmail.com>
2023-10-05 02:20:05 +0530

    Quest Tree redirection implemented

--- Algorithmia/Assets/Scripts/Simulation/ArrayLevels.cs
+++ Algorithmia/Assets/Scripts/Simulation/ArrayLevels.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class ArrayLevels : MonoBehaviour
 {
+    [SerializeField] private GameObject noEnergyMenu;
+    [SerializeField] private SimManager simulationManager;
+    
     public void LoadLevels(string levelName)
     {
-        SceneManager.LoadScene(levelName);
+        if (simulationManager.energyLevel - 20 <= 0)
+        {
+            noEnergyMenu.SetActive(true);
+            return;
+        }
+
+        //Todo - playtime saving
+        simulationManager.energyLevel -= 20;
+        SceneManager.LoadSceneAsync(levelName);
+    }
+
+    public void ChangeLoadTreePrefs()
+    {
+        PlayerPrefs.SetInt("LoadArrayTree", 0);
     }
 }
