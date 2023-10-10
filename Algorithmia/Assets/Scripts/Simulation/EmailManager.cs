e21c6fbfefdfd289bbaec558980639e900e809c5
Kavindu <kavindupriyanath@gmail.com>
2023-10-10 21:57:03 +0530

    Email Manager Added

---
+++ Algorithmia/Assets/Scripts/Simulation/EmailManager.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmailManager : MonoBehaviour
+{
+    [SerializeField] private SimManager simulationManager;
+
+    private void Update()
+    {
+        
+    }
+}
