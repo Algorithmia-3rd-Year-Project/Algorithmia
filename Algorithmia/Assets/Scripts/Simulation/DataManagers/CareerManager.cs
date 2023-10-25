29d4559063ac2e97e90b5246a67bb77581c9eedd
Kavindu <kavindupriyanath@gmail.com>
2023-10-25 20:37:27 +0530

    Single Job Info Added

--- Algorithmia/Assets/Scripts/Simulation/DataManagers/CareerManager.cs
+++ Algorithmia/Assets/Scripts/Simulation/DataManagers/CareerManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
 public class CareerManager : MonoBehaviour
 {
@@ -14,9 +15,20 @@ public class CareerManager : MonoBehaviour
     [SerializeField] private GameObject careerPrefab;
 
     [SerializeField] private Transform careerParent;
+
+    private List<Button> careerButtonList;
     
-    private void Start()
+    //Job Info Panel
+    [SerializeField] private GameObject careerInfoWindow;
+    [SerializeField] private TMP_Text jobNameText;
+    [SerializeField] private TMP_Text jobFieldText;
+    [SerializeField] private TMP_Text jobSalaryText;
+    [SerializeField] private TMP_Text jobCompanyText;
+    
+    private void Awake()
     {
+        careerButtonList = new List<Button>();
+        
         //string jsonPath = Application.dataPath + "/Data/Careers.json";
         string jsonPath = Application.streamingAssetsPath + "/Careers.json";
         string json = File.ReadAllText(jsonPath);
@@ -27,11 +39,17 @@ public class CareerManager : MonoBehaviour
         {
             GameObject singleCareer = Instantiate(careerPrefab, careerParent);
             SingleJob singleCareerScript = singleCareer.GetComponent<SingleJob>();
+            Button singleJobButton = singleCareer.GetComponent<Button>();
+            careerButtonList.Add(singleJobButton);
             
             //Assigning data to the prefab
             singleCareerScript.jobNameText.text = career.jobName;
             singleCareerScript.fieldNameText.text = career.field;
             singleCareerScript.salaryText.text = career.salary;
+            singleCareerScript.jobName = career.jobName;
+            singleCareerScript.fieldName = career.field;
+            singleCareerScript.salaryAmount = career.salary;
+            singleCareerScript.companyName = career.employer;
 
             string imagePath = "Careers/" + career.jobIcon;
             Sprite loadedImage = LoadSprite(imagePath);
@@ -41,11 +59,42 @@ public class CareerManager : MonoBehaviour
 
             //Debug.Log($"Career ID: {career.id}, jobname: {career.jobName}, Field: {career.field}");
         }
+        
+    }
+
+    private void Start()
+    {
+        if (careerButtonList.Count > 0)
+        {
+            AddListeners();
+        }
+        
     }
 
     private Sprite LoadSprite(string path)
     {
         Sprite loadedSprite = Resources.Load<Sprite>(path);
         return loadedSprite;
+    }
+
+    private void AddListeners()
+    {
+        foreach (Button btn in careerButtonList)
+        {
+            btn.onClick.AddListener(() => ClickedOnJob());
+        }
+    }
+
+    public void ClickedOnJob()
+    {
+        GameObject jobObject = EventSystem.current.currentSelectedGameObject;
+        SingleJob job = jobObject.GetComponent<SingleJob>();
+        Debug.Log(job.jobName);
+        
+        careerInfoWindow.SetActive(true);
+        jobNameText.text = job.jobName;
+        jobFieldText.text = job.fieldName;
+        jobSalaryText.text = job.salaryAmount;
+        jobCompanyText.text = job.companyName;
     }
 }
